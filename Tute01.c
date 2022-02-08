c4e93d469b12c648bc9def81518cb31773fdfba7
Sathmi <99236284+IT21925290@users.noreply.github.com>
2022-02-08 06:10:20 +0000

    Answers for the 4 Exercises

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,26 @@
 
 int main() {
   
+  int mark1, mark2,total;
+	float average;
+
+  //ENTER FIRST mark1
+  printf("Please Enter the Subject 1 :");
+  scanf("%d", &mark1);
+
+  //Enter second mark
+	printf("Please enter the Subject 2 :");
+	scanf("%d", &mark2);
+
+  //Calculate the total
+	total = mark1+mark2;
+
+  //Calculate the average
+	average = total/2;
+
+  //Print the average
+  printf("Average marks : %.1f", average);
+  
   return 0;
-}
+}//end of main function
 
--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,20 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int n, sum = 0, count = 0;
+
+  // Input the value for n
+  printf("Input the number for n :");
+  scanf("%d", &n);
+
+  while (count <= n){
+    sum = sum + count;
+    count++;    
+  }
+
+  //Printing the sum of the numbers
+  printf("Sum = %d", sum);
+
   return 0;
 }
 
